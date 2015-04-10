b6eff3f075c6f5defa618fc29039e4c162c16c72
waiso <waiso.ti@gmail.com>
2015-04-09 22:44:28 -0300

    Novos exercícios.

--- src/br/com/logica/tecnicas/programacao/exercicios00010/Exercicicio01.java
+++ src/br/com/logica/tecnicas/programacao/exercicios00010/Exercicicio01.java
@@ -1,15 +1,17 @@
 package br.com.logica.tecnicas.programacao.exercicios00010;
 
+import javax.swing.JOptionPane;
+
 /**
  * @author Alberto Cerqueira
  * @email alberto.cerqueira1990@gmail.com
- * @date 2015/04/08
+ * @date 2015/04/09
  */
 public class Exercicicio01 {
 
 	public static void main(String[] args) {
-		Exercicicio01 exe00 = new Exercicicio01();
-		exe00.pesquisaSalarialProfessores();
+		Exercicicio01 exe01 = new Exercicicio01();
+		exe01.pesquisaSalarialProfessores();
 	}
 	
 	/**
@@ -26,6 +28,34 @@ public class Exercicicio01 {
 	 * A leitura dos dados em sua função para quando for digitado um número negativo para o número de matrícula do professor.
 	 */
 	public void pesquisaSalarialProfessores() {
-		//TODO
+		int matricula = 0, maiorValorMatricula = 0, i = 0, quantidadeLocais = 0;;
+		double maiorValor = 0;
+		do {
+			matricula = Integer.parseInt(JOptionPane.showInputDialog("Digite a matr\u00edcula do " + (i + 1) + "° professor:"));
+			if (matricula >= 0) {
+				int quantidadeAulasLocais = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de lugares que o " + (i + 1) + "° professor ministra:"));
+				double[] ganhoProfessor = new double[quantidadeAulasLocais];
+				double valorTotal = 0;
+				for (int y = 0; y < quantidadeAulasLocais; y++) {
+					int quantidadeAulaLocal = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de aulas semanais que o professor ministra no " + (y + 1) + "° local:"));
+					double valor = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor/hora da aula que o professor ministra no " + (y + 1) + "° local:"));
+					int quantidadeSemanas = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de semanas que o professor vai ministrar no " + (y + 1) + "° local:"));
+					double valorCalculado = valor * (quantidadeAulaLocal * quantidadeSemanas);
+					ganhoProfessor[y] = valorCalculado;
+					if (valorCalculado > maiorValor) {
+						maiorValor = valorCalculado;
+						maiorValorMatricula = matricula;
+					}
+					
+					System.out.println("O professor com matr\u00edcula " + matricula + " fatura " + valorCalculado + " no " + (y + 1) + "° local.");
+					valorTotal += valorCalculado;
+				}
+				System.out.println("O professor com matr\u00edcula " + matricula + " fatura " + valorTotal + " ao todo.");
+				quantidadeLocais += quantidadeAulasLocais;
+				i++;
+			}
+		} while (matricula >= 0);
+		System.out.println("O maior valor ganho por um professor \u00e9 " + maiorValor + " o qual possui matr\u00edcula: " + maiorValorMatricula);
+		System.out.println("A m\u00e9dia de locais que os professores ministram aulas \u00e9: " + (quantidadeLocais / i));
 	}
 }
